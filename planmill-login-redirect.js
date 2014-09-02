493d0a72129d6d7c362509601dc7fdb0f267c5be
Janne K <0x022b@gmail.com>
2014-09-02 07:32:22 +0300

    Fixed a misspelled website in script name

--- planmill-login-redirect.js
+++ planmill-login-redirect.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Planmill Login Redirect
+// @name         PlanMill Login Redirect
 // @description  Redirects an expired session to a login page that doesn't fail.
-// @version      1.0.1
+// @version      1.0.2
 // @homepage     https://bitbucket.org/scoobadog/tampermonkey/
 // @updateURL    https://bitbucket.org/scoobadog/tampermonkey/raw/master/planmill-login-redirect.js
 // @match        https://online.planmill.com/*/*
