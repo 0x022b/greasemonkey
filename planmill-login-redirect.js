c4f464bae943a7b1329493f442eefae39fca0107
Janne K <0x022b@gmail.com>
2014-09-22 09:10:43 +0300

    Completely rewritten redirection logic

--- planmill-login-redirect.js
+++ planmill-login-redirect.js
@@ -1,16 +1,20 @@
 // ==UserScript==
 // @name         PlanMill Login Redirect
 // @description  Redirects an expired session to a login page that doesn't fail.
-// @version      1.0.2
+// @version      1.1.0
 // @homepage     https://bitbucket.org/scoobadog/tampermonkey/
 // @updateURL    https://bitbucket.org/scoobadog/tampermonkey/raw/master/planmill-login-redirect.js
-// @match        https://online.planmill.com/*/*
+// @include      /^https://online\.planmill\.com/.+/.+$/
 // @grant        none
 // @run-at       document-body
 // ==/UserScript==
-var el = document.getElementById("logindiv");
-var query = window.location.search;
-if (el !== null && query !== "") {
-	var url = window.location.href.replace(query, "");
-	window.location.replace(url)
+if (document.getElementById("logindiv") !== null) {
+	var path = window.location.pathname;
+	var url = window.location.href
+		.replace(path, path.substr(0, path.indexOf("/", 1)))
+		.replace(window.location.search, "") + "/";
+
+	if (url.length < window.location.href.length) {
+		window.location.replace(url)
+	}
 }
