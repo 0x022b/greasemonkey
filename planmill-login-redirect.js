0b8a2af9da747c07f2ab0a04be943a5f7496e8b8
Janne K <0x022b@gmail.com>
2015-03-05 09:28:18 +0200

    Added a redirect from security check page back to login page

--- planmill-login-redirect.js
+++ planmill-login-redirect.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name         PlanMill Login Redirect
 // @description  Redirects an expired session to a login page that doesn't fail.
-// @version      1.1.0
+// @version      1.2.0
 // @homepage     https://bitbucket.org/scoobadog/tampermonkey/
 // @updateURL    https://bitbucket.org/scoobadog/tampermonkey/raw/master/planmill-login-redirect.js
 // @include      /^https://online\.planmill\.com/.+/.+$/
 // @grant        none
 // @run-at       document-body
 // ==/UserScript==
-if (document.getElementById("logindiv") !== null) {
-	var path = window.location.pathname;
+var path = window.location.pathname;
+if (document.getElementById("logindiv") !== null
+	|| path.endsWith("j_security_check")) {
+
 	var url = window.location.href
 		.replace(path, path.substr(0, path.indexOf("/", 1)))
 		.replace(window.location.search, "") + "/";
