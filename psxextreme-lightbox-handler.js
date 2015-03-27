b5574308b562e64eddf214dbfdcf8b2ab9124835
Janne K <0x022b@gmail.com>
2015-03-27 23:10:39 +0200

    Added PSX Extreme Lightbox Handler script

---
+++ psxextreme-lightbox-handler.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         PSX Extreme Lightbox Handler
+// @description  Prevents opening of the lightbox on PSX Extreme.
+// @version      1.0.0
+// @homepage     https://bitbucket.org/scoobadog/tampermonkey/
+// @updateURL    https://bitbucket.org/scoobadog/tampermonkey/raw/master/psxextreme-lightbox-handler.js
+// @include      /^http://www\.psxextreme\.com/$
+// @grant        none
+// @run-at       document-body
+// ==/UserScript==
+if (set_cookie) {
+    set_cookie("fb_like", "false", 1);
+}
