851ab567e93ead185d1b29ef394c37c98fd35a77
Janne K <0x022b@gmail.com>
2015-06-26 23:13:19 +0300

    Added a script that fixes cross origin Javascript errors on Movescount

---
+++ movescount-cross-origin-xmlhttprequest.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @author       Janne K
+// @name         Movescount Cross-Origin XMLHttpRequest
+// @description  Fixes "No 'Access-Control-Allow-Origin' header is present on the requested resource" errors on Movescount.
+// @version      1.0.0
+// @homepage     https://bitbucket.org/scoobadog/tampermonkey/
+// @updateURL    https://bitbucket.org/scoobadog/tampermonkey/raw/master/movescount-cross-origin-xmlhttprequest.user.js
+// @include      /^https?://www\.movescount\.com/moves/move.+$/
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+suunto.move.samplesUrl = undefined;
+suunto.move.trackUrl = undefined;
