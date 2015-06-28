f510c7a838a75f1fada2ffd665ccbe1ef9d38f8d
Janne K <0x022b@gmail.com>
2015-06-28 11:23:17 +0300

    Run the script when the body element exists
    Javascript variable "suunto" didn't necessarily exist when the script was run.

--- movescount-cross-origin-xmlhttprequest.user.js
+++ movescount-cross-origin-xmlhttprequest.user.js
@@ -2,12 +2,12 @@
 // @author       Janne K
 // @name         Movescount Cross-Origin XMLHttpRequest
 // @description  Fixes "No 'Access-Control-Allow-Origin' header is present on the requested resource" errors on Movescount.
-// @version      1.0.0
+// @version      1.1.0
 // @homepage     https://bitbucket.org/scoobadog/tampermonkey/
 // @updateURL    https://bitbucket.org/scoobadog/tampermonkey/raw/master/movescount-cross-origin-xmlhttprequest.user.js
 // @include      /^https?://www\.movescount\.com/moves/move.+$/
 // @grant        none
-// @run-at       document-start
+// @run-at       document-body
 // ==/UserScript==
 
 suunto.move.samplesUrl = undefined;
