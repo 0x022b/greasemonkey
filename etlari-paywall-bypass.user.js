f7ffc89a930b06ac839d7680d378eb3768343226
Janne K <0x022b@gmail.com>
2016-12-09 14:27:46 +0200

    Fix a timing issue with Greasemonkey
    Greasemonkey ran the script too late with document-body even though it worked fine with Tampermonkey.

--- etlari-paywall-bypass.user.js
+++ etlari-paywall-bypass.user.js
@@ -2,12 +2,12 @@
 // @author       Janne K
 // @name         Etlari Paywall Bypass
 // @description  Bypasses Etlari's paywall
-// @version      1.0.0
+// @version      1.0.1
 // @homepage     https://github.com/scoobadog/tampermonkey/
 // @downloadURL  https://raw.githubusercontent.com/scoobadog/tampermonkey/master/etlari-paywall-bypass.user.js
 // @include      /^https?:\/\/www\.ess\.fi\/.+$/
 // @grant        none
-// @run-at       document-body
+// @run-at       document-start
 // ==/UserScript==
 
 Object.defineProperty(document, "referrer", {
