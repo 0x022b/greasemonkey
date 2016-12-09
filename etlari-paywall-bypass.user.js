b49305964f64ccce4fb845fef5cb5c50792b017b
Janne K <0x022b@gmail.com>
2016-12-09 14:06:52 +0200

    Add Etlari Paywall Bypass script

---
+++ etlari-paywall-bypass.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author       Janne K
+// @name         Etlari Paywall Bypass
+// @description  Bypasses Etlari's paywall
+// @version      1.0.0
+// @homepage     https://github.com/scoobadog/tampermonkey/
+// @downloadURL  https://raw.githubusercontent.com/scoobadog/tampermonkey/master/etlari-paywall-bypass.user.js
+// @include      /^https?:\/\/www\.ess\.fi\/.+$/
+// @grant        none
+// @run-at       document-body
+// ==/UserScript==
+
+Object.defineProperty(document, "referrer", {
+	get: function() { return "https://t.co"; }
+});
