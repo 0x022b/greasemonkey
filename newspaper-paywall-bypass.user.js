a917a95ef7c47806e4a58b8c1721279c34dae3d7
Janne K <0x022b@gmail.com>
2016-12-14 14:39:12 +0200

    Add more generic paywall bypass script
    Deprecate etlari-paywall-bypass.user.js script by instructing it to update to the new script.

--- newspaper-paywall-bypass.user.js
+++ newspaper-paywall-bypass.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @author       Janne K
-// @name         Etlari Paywall Bypass
-// @description  Bypasses Etlari's paywall
-// @version      1.0.2
+// @name         Newspaper Paywall Bypass
+// @description  Bypasses paywall on Finnish newspapers' websites
+// @version      1.1.0
 // @homepage     https://github.com/scoobadog/tampermonkey/
 // @downloadURL  https://raw.githubusercontent.com/scoobadog/tampermonkey/master/newspaper-paywall-bypass.user.js
-// @include      /^https?:\/\/www\.ess\.fi\/.+$/
+// @include      /^https?:\/\/www\.(?:ess|hs)\.fi\/.+$/
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
