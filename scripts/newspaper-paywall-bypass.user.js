3d588a3597a791e5217b3397865b35c03af3d567
Janne K <0x022b@gmail.com>
2016-12-29 12:28:36 +0200

    Split domains into separate include rules

--- scripts/newspaper-paywall-bypass.user.js
+++ scripts/newspaper-paywall-bypass.user.js
@@ -5,7 +5,8 @@
 // @version      1.1.4
 // @homepage     https://github.com/scoobadog/greasemonkey
 // @downloadURL  https://raw.githubusercontent.com/scoobadog/greasemonkey/master/scripts/newspaper-paywall-bypass.user.js
-// @include      /^https?:\/\/www\.(?:ess|hs)\.fi\/.+$/
+// @include      /^https?://www\.ess\.fi/.+$/
+// @include      /^https?://www\.hs\.fi/.+$/
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
