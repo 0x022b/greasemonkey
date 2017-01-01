89f7eb6679869fb5beedb42ff9792185663c8026
Janne K <0x022b@gmail.com>
2017-01-01 18:13:40 +0200

    Make the script more reliable
    Minor improvements here and there. Also fixes the issue with the
    button disappearing.

--- scripts/dropbox-direct-download-links.user.js
+++ scripts/dropbox-direct-download-links.user.js
@@ -2,34 +2,36 @@
 // @author       Janne K
 // @name         Dropbox Direct Download Links
 // @description  Adds a button to Dropbox shared folder that lists direct download links.
-// @version      1.0.2
+// @version      1.1.0
 // @homepage     https://github.com/scoobadog/greasemonkey
 // @downloadURL  https://raw.githubusercontent.com/scoobadog/greasemonkey/master/scripts/dropbox-direct-download-links.user.js
-// @include      /^https?://www\.dropbox\.com/.+\?dl=0$/
+// @include      /^https?://www\.dropbox\.com/.+\?dl=0(?:#.*)?$/
 // @grant        none
-// @run-at       document-start
+// @run-at       document-body
 // ==/UserScript==
 
 var d = document, o;
 o = new MutationObserver(function () {
-	if (d.querySelector(".react-title-bar")) {
+	if (!d.querySelector("#links-button")) {
 		var e = d.createElement("a");
-		e.href = "#";
+		e.id = "links-button";
+		e.href = "#links";
 		e.className = "button-primary control__button";
 		e.innerHTML = "Links";
 		e.addEventListener("click", function () {
-			var l = d.createElement("div");
-			l.className = "sl-gallery-container";
-			d.querySelectorAll(".sl-file-link").forEach(function (a) {
-				l.innerHTML += a.href
-					.replace("www.dropbox.com", "dl.dropboxusercontent.com")
-					.replace("?dl=0", "") + "<br/>";
-			});
-			d.querySelector(".sl-page-body").appendChild(l);
+			if (!d.querySelector("#links")) {
+				var l = d.createElement("div");
+				l.id = "links";
+				l.className = "sl-gallery-container";
+				d.querySelectorAll(".sl-file-link").forEach(function (a) {
+					l.innerHTML += a.protocol +
+						"//dl.dropboxusercontent.com" + a.pathname + "<br/>";
+				});
+				d.querySelector(".sl-page-body").appendChild(l);
+			}
 		});
 		var c = d.querySelector(".react-title-bar__controls");
 		c.insertBefore(e, c.lastChild);
-		o.disconnect();
 	}
 });
-o.observe(d.querySelector("html"), { childList: true, subtree: true });
+o.observe(d.querySelector(".preview-box"), { childList: true, subtree: true });
